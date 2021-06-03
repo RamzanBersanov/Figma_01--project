069813eda756d3d1b82f2e1d2a6bdad94bf2a276
RamzanBersanov <65899466+RamzanBersanov@users.noreply.github.com>
2021-06-03 16:06:08 +0300

    Добавил jquery для product

--- js/main.js
+++ js/main.js
@@ -85,3 +85,26 @@ const menuLinks = document.querySelectorAll('.menu__link[data-goto]');
             }
         }
     }
+
+
+    
+ 
+
+    $(function(){
+        $(".color_wrap ul li").each(function(item) {
+            var color = $(this).attr("data-color");
+            $(this).css("backgroundColor", color);
+            
+        }) 
+    
+        $(".color_wrap ul li").each(function(item) {
+            $(this).click(function(){
+                $(this).parents(".product_item")
+                .find(".color_wrap ul li").removeClass("active");
+                $(this).addClass("active");
+                
+                var imgsrc = $(this).attr("data-src");
+                $(this).parents(".product_content").find("img").attr("src", imgsrc);
+            })
+        })
+    })
